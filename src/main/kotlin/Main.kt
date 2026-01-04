d2bfa37f7e1c051afcaa73e65c92dd2a893abaed
andrewOldGuapchich <i@andrewkononov.ru>
2026-01-04 18:18:12 +0300

    Updated bot

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,6 @@
 package com.andrew.tg
 
 import com.andrew.tg.bot.SimpleKotlinBot
-import com.andrew.tg.service.LogMessageLevel
 import com.andrew.tg.service.LoggerService
 import org.telegram.telegrambots.meta.TelegramBotsApi
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
@@ -9,14 +8,14 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
 class Main
 val logger = LoggerService(Main::class.java)
 fun main() {
-    logger.writeLogMessage(LogMessageLevel.INFO, "#####################################")
-    logger.writeLogMessage(LogMessageLevel.INFO, "######### PICTURE-BOT START #########")
-    logger.writeLogMessage(LogMessageLevel.INFO, "#####################################")
+    logger.info("#####################################")
+    logger.info("######### PICTURE-BOT START #########")
+    logger.info("#####################################")
     try {
         val botsApi = TelegramBotsApi(DefaultBotSession::class.java)
         botsApi.registerBot(SimpleKotlinBot())
-        logger.writeLogMessage(LogMessageLevel.INFO, "Picture-Bot started successful.")
+        logger.info("Picture-Bot started successful.")
     } catch (e: Exception) {
-        logger.writeLogMessage(LogMessageLevel.INFO, "Picture-Bot started failed. ${e.message}")
+        logger.info("Picture-Bot started failed. ${e.message}")
     }
 }
